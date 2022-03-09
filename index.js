9d73c5e0e685c77bebe1a2908e3377d0057fb2bd
MrWilliamA <will.archer@hotmail.com>
2022-03-09 18:35:15 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+ 
+
+let currentUser = "will";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"; 
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
